c843cf2238e4ddc26d9741d4f40a9c47d376c25e
tamia princee <tamia.t.prince@gmail.com>
2023-06-02 09:29:49 -0400

    Azure key

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ from utils.snowddl import Snowddl
 from utils.snowchat_ui import reset_chat_history, extract_code, message_func, is_sql_query
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
-
+openai.api_type = "azure"
+openai.api_base = "https://ai-accerator.openai.azure.com/"
+openai.api_version = "2023-03-15-preview"
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+MAX_INPUTS = 5
 #Limit of 3 inputs because gpt-3.5-turbo only allows 3 per min. 
-MAX_INPUTS = 3
+
 chat_history = []
 
 st.set_page_config(
